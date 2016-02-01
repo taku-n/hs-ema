0f658082d44ed7ec099441432f90df90aa41737f
yoshino <hoge@hoge.com>
2016-02-01 20:06:49 +0900

    reduced edition

--- ema.hs
+++ ema.hs
@@ -1,8 +1,6 @@
 {-# OPTIONS -Wall #-}
 
 ema :: [Double] -> Int -> [Double]
-ema xs n =
-
-nextEMA :: Double -> Double -> Int -> Double
-nextEMA y' s n = a * y' + (1 - a) * s
-    where a = 2.0 / (fromIntegral n + 1.0)
+ema (s:ys) n = reverse $ foldl getNextEMA [s] ys
+    where getNextEMA all@(s:_) y' = (a * y' + (1 - a) * s):all
+          a                       = 2.0 / (fromIntegral n + 1.0)
